55c9869cb035ef7e0ea09c130e951d56b5913df8
Administrator <Administrator@DESKTOP-IBPV0HH>
2020-12-29 20:30:01 +0800

    conggg

--- sp02-itemservice/src/main/java/com/k/sp02/item/service/ItemServiceImpl.java
+++ sp02-itemservice/src/main/java/com/k/sp02/item/service/ItemServiceImpl.java
@@ -3,6 +3,7 @@ package com.k.sp02.item.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Service;
 
 import com.k.sp01.pojo.Item;
@@ -13,6 +14,7 @@ import lombok.extern.slf4j.Slf4j;
 
 @Service
 @Slf4j
+@RefreshScope
 public class ItemServiceImpl implements ItemService{
 
 	@Override
